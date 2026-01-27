e359a2cff9d02f12e7bc37b59f05519986b14a0c
Aleina Rossui <admin@promoflow.net>
2026-01-27 18:19:44 -0500

    mensaje

--- public/customs/punto.js
+++ public/customs/punto.js
@@ -381,7 +381,7 @@ function obtenerDetallesProducto(idProducto) {
       data: { id_producto: idProducto }
   })
   .done(function (response) {
-    //alert("3. Resputesta punto.js linea 384");
+    alert("3. Resputesta punto.js linea 384");
       if (response.success) {
           const { nombre, talla, precio, stock, id_sucursal } = response.respuesta;
 
