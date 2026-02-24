cd15cc93d36dbeab0b0608f555a72ac1e9627424
oscarbeltranc <oscarbeltran05@gmail.com>
2026-02-23 19:18:45 -0500

    alerta
    verificación cantidad de producto

--- public/customs/punto.js
+++ public/customs/punto.js
@@ -326,6 +326,7 @@ $(document).ready(function () {
   }
 
   function verificarEstado(cantidadProducto) {
+    alert("cantidadProducto: " + cantidadProducto); 
     if (parseInt(cantidadProducto) > 0) {
       estadoProducto = parseInt(2)
     } else {
